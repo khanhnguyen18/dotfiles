4ce34d105b5f2a826721f7269b275d430e692185
khanh-nguyen <khanh.nguyen@adurolife.com>
2022-07-05 23:17:16 +0700

    Allow f5 to reload

--- .emacs.d.gnu/config.el
+++ .emacs.d.gnu/config.el
@@ -1,2 +1,4 @@
+(global-set-key (kbd "<f5>") (lambda() (interactive) (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))))
+
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
